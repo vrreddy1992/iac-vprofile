67f8b046b9d26794ec987f4fc71e62dabca8d61e
vrreddy1992 <vrreddy1992@gmail.com>
2025-01-20 13:05:51 -0600

    stage-work

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
+    bucket = "terraform-state-raja"
+    key    = "terraform-state"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
